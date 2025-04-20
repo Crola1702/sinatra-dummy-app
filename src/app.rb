823e33645ddfa4e3e0b8e9ecefab96ec9ee6faaf
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2025-04-19 19:52:57 -0500

    Use errors module
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- src/app.rb
+++ src/app.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'json'
 require_relative 'models'
+require_relative 'errors'
 
 get '/' do
   'Hello world!'
@@ -11,11 +12,11 @@ end
 # Get user
 get '/users/:id' do
   user = User.find_by(id: params['id'])
-  raise 'User not found' if user.nil?
+  raise NotFoundError, 'User not found' if user.nil?
 
   [200, {}, user.to_json]
-rescue RuntimeError => e
-  [404, {}, JSON.dump({ message: e })]
+rescue ApiError => e
+  [e.status_code, {}, JSON.dump({ message: e })]
 end
 
 # Create user
@@ -24,44 +25,44 @@ post '/users' do
   request_body = request.body.read
   data = JSON.parse(request_body, { symbolize_names: true })
 
-  raise 'Must specify a name' unless data.key? :name
+  raise BadRequestError, 'Must specify a name' unless data.key? :name
 
   user = User.create(name: data[:name])
 
   [201, {}, user.to_json]
-rescue RuntimeError => e
-  [400, {}, JSON.dump({ message: e })]
+rescue ApiError => e
+  [e.status_code, {}, JSON.dump({ message: e })]
 end
 
 # Get messages
 get '/users/:id/messages' do
   user = User.find_by(id: params['id'])
-  raise "User with id #{params['id']} does not exist" if user.nil?
+  raise NotFoundError, "User with id #{params['id']} does not exist" if user.nil?
 
   messages = Message.where('receiver_id = :rec_id or sender_id = :send_id', { rec_id: user.id, send_id: user.id })
   [200, {}, messages.to_json]
-rescue StandardError => e
-  [400, {}, JSON.dump({ message: e })]
+rescue ApiError => e
+  [e.status_code, {}, JSON.dump({ message: e })]
 end
 
 # Create message
 post '/users/:id/messages' do
   sender = User.find_by(id: params['id'])
-  raise "User with id #{params['id']} does not exist" if sender.nil?
+  raise NotFoundError, "User with id #{params['id']} does not exist" if sender.nil?
 
   request.body.rewind
   request_body = request.body.read
   data = JSON.parse(request_body, { symbolize_names: true })
 
   %i[to content].each do |k|
-    raise "Missing '#{k}' key" unless data.key? k
+    raise BadRequestError, "Missing '#{k}' key" unless data.key? k
   end
 
   receiver = User.find_by(id: data[:to])
-  raise "User with id #{params['id']} does not exist" if receiver.nil?
+  raise NotFoundError, "User with id #{params['id']} does not exist" if receiver.nil?
 
   msg = Message.create(sender_id: sender.id, receiver_id: receiver.id, content: data[:content])
   [201, {}, msg.to_json]
-rescue StandardError => e
-  [400, {}, JSON.dump({ message: e })]
+rescue ApiError => e
+  [e.status_code, {}, JSON.dump({ message: e })]
 end
---
+++ src/errors.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ApiError < StandardError
+  def status_code
+    raise NotImplementedError
+  end
+end
+
+class NotFoundError < ApiError
+  def status_code
+    404
+  end
+end
+
+class BadRequestError < ApiError
+  def status_code
+    400
+  end
+end
